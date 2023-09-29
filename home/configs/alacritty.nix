137af34b1faae4039e40e67faf16fdb998313b73
Jeppe Allerslev <jeppeallerslev@gmail.com>
2023-09-29 17:37:13 +0200

    moved alacritty config to nix

---
+++ home/configs/alacritty.nix
@@ -0,0 +1,21 @@
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 11;
+        normal.family = "MesloLGS NF";
+        normal.style = "Regular";
+
+        bold.family = "MesloLGS NF";
+        bold.style = "Bold";
+
+        italic.family = "MesloLGS NF";
+        italic.style = "Italic";
+
+        "bold_italic".family = "MesloLGS NF";
+        "bold_italic".style = "Bold Italic";
+      };
+    }; 
+  };
+}
