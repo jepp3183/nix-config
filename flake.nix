a5080b3a24270f9ce1156aa8cda80bd9c6f33a22
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-04-07 17:37:46 +0200

    local commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Main NixOS flake for envy laptop";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...} @ inputs: 
+    let
+      system = "x86_64-linux";
+    in {
+    
+      nixosConfigurations = {
+        "nixos-envy" = nixpkgs.lib.nixosSystem {
+
+          inherit system;
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      }; 
+    };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+   
+  environment.systemPackages = with pkgs; [
+    neovim
+    kitty
+    git
+
+    (waybar.overrideAttrs (oldAttrs: {
+    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; }))
+
+    dunst #notifications
+    libnotify #dunst needs this
+    networkmanagerapplet
+    pavucontrol
+    brightnessctl
+    playerctl
+    swaylock
+    sddm-chili-theme
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking.hostName = "nixos-envy"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Copenhagen";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_DK.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    displayManager.sddm = {
+      enable = true;
+      theme = "chili";
+    };
+    
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jeppe = {
+    isNormalUser = true;
+    description = "Jeppe Allerslev";
+    extraGroups = [ "networkmanager" "wheel" "input" ];
+    shell = pkgs.fish;
+  };
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+  environment.shells = [ pkgs.fish ];
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # environment.variables.EDITOR = "nvim";
+
+  hardware.opengl.enable = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  services.acpid= {
+    enable = true;
+    logEvents = true;
+  };
+  security.pam.services.swaylock.text = ''
+  auth include login
+  '';
+  
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
