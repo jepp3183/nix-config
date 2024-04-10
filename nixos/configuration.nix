3f2c3242d45403cd703b9db69efff27e82192b95
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-04-10 23:09:42 +0200

    enable nix-ld

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,6 +29,12 @@
     sddm-chili-theme
   ];
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
